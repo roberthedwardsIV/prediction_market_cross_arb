7aecaa996c6703b517a82a644850e940132c2eb1
roberthedwardsIV <125324864+roberthedwardsIV@users.noreply.github.com>
2026-08-27 22:20:19 -0400

    second commit

--- build/CMakeFiles/fx_lookup.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for fx_lookup.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/twsapi.dir/Users/hitchedwards/Downloads/twsapi_macunix/IBJts/source/cppclient/client/StdAfx.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/twsapi.dir/Users/hitchedwards/Downloads/twsapi_macunix/IBJts/source/cppclient/client/StdAfx.cpp.o: \
-  /Users/hitchedwards/Downloads/twsapi_macunix/IBJts/source/cppclient/client/StdAfx.cpp \
-  /Users/hitchedwards/Downloads/twsapi_macunix/IBJts/source/cppclient/client/StdAfx.h
